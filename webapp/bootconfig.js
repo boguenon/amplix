ac32c29a66ab6d2c8756d5c197befe237624a2d3
boguenon <boguenon@gmail.com>
2020-05-05 12:57:55 -0400

    build revision 1179-4.13-106-g9367527a, 202005051255

--- webapp/bootconfig.js
+++ webapp/bootconfig.js
@@ -1,5 +1,5 @@
 ig$.bootconfig = {
-	"cache": "202005051044",
+	"cache": "202005051255",
     "boot": {
         "framework": [
             "./js/framework.min.js"
--- webapp/config.js
+++ webapp/config.js
@@ -83,8 +83,8 @@ var ig$ = {
     ],
 	echarts_theme: "amplix",
 	appInfo: {
-		date: "202005051044",
-		web_revision: "1178-4.13-105-g8392a494"
+		date: "202005051255",
+		web_revision: "1179-4.13-106-g9367527a"
 	}
     // hide_report_help: true
 };
