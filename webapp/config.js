79772c31a318e3c5221a65f5f5b0e0fe7b847071
boguenon <boguenon@gmail.com>
2020-12-29 14:04:29 -0500

    build revision 1986-4.13-913-gc60859ee, 202012291402

--- webapp/config.js
+++ webapp/config.js
@@ -70,10 +70,10 @@
     ],
 	echarts_theme: "amplix",
 	appInfo: {
-		date: "202012291346",
+		date: "202012291402",
 		appversion: "",
 		apprelease: "",
-		web_revision: "1983-4.13-910-gbe8f4869"
+		web_revision: "1986-4.13-913-gc60859ee"
 	},
 	$l_:0
 };
