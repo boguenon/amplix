f4066d5a12c48cc942eefce9d027e7627fe53179
boguenon <boguenon@gmail.com>
2022-01-21 17:33:59 -0800

    build revision 2586-version_20201215-702-gb84322aa, 202201211731

--- webapp/bootconfig.js
+++ webapp/bootconfig.js
@@ -1,5 +1,5 @@
 ﻿ig$.bootconfig = {
-	"cache": "202201211651",
+	"cache": "202201211731",
     "boot": {
         "framework": [
             "./js/framework.min.js"
--- webapp/config.js
+++ webapp/config.js
@@ -71,10 +71,10 @@
     ],
 	echarts_theme: "amplix",
 	appInfo: {
-		date: "202201211651",
+		date: "202201211731",
 		appversion: "",
 		apprelease: "",
-		web_revision: "2584-version_20201215-700-g173ec05c"
+		web_revision: "2586-version_20201215-702-gb84322aa"
 	},
 	$l_:0
 };
