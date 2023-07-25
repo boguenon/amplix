ffa4b9c9a4b1e0682c7c2c40c00dd4e02a6cc128
boguenon <boguenon@gmail.com>
2023-07-25 10:00:36 -0400

    build revision 3318-ver5.01-192-gff1693ec, 202307250953

--- webapp/bootconfig.js
+++ webapp/bootconfig.js
@@ -1,5 +1,5 @@
 ﻿ig$.bootconfig = {
-	"cache": "202306272202",
+	"cache": "202307250953",
     "boot": {
         "framework": [
             "./js/framework.min.js"
@@ -108,6 +108,9 @@
         ],
 		"mg_meta": [
 			"./js/mg_meta.min.js"
-		]
+		],
+        "mongo": [
+            "./js/mongo.min.js"
+        ]
     }
 };
--- webapp/config.js
+++ webapp/config.js
@@ -6,7 +6,7 @@
     appbg: "bg_7186.png",
     companydomain: "http://www.amplixbi.com",
     applink: "launcher/viewer.jsp?objid={objid}",
-    copy: "&copy; 2005-" + "202306272202".substring(0, 4) + " amplixbi.com Inc. <br />ALL RIGHTS RESERVED. <br />amplix. Confidential Information",
+    copy: "&copy; 2005-" + "202307250953".substring(0, 4) + " amplixbi.com Inc. <br />ALL RIGHTS RESERVED. <br />amplix. Confidential Information",
     intropage: "navi_intro",
     timer_rsn: 300000,
     timer_ping: 0,
@@ -71,10 +71,10 @@
     ],
 	echarts_theme: "amplix",
 	appInfo: {
-		date: "202306272202",
+		date: "202307250953",
 		appversion: "",
 		apprelease: "",
-		web_revision: "3304-ver5.01-178-gf709505b"
+		web_revision: "3318-ver5.01-192-gff1693ec"
 	},
 	$l_:0
 };
